88cd3197f460acc791f3827e13c79339843542e9
abuhamza-git <abuhamza-git@users.noreply.github.com>
2026-09-18 02:01:53 +0300

    chore: Bump version to 1.0.6

--- desktop-app/build.gradle.kts
+++ desktop-app/build.gradle.kts
@@ -38,7 +38,7 @@ compose.desktop {
         mainClass = "com.pilcrowmd.desktop.MainKt"
         nativeDistributions {
             packageName = "PilcrowMD"
-            packageVersion = "1.0.5"
+            packageVersion = "1.0.6"
             description = "A beautiful Markdown reader & editor"
             modules("java.instrument", "jdk.unsupported")
 
